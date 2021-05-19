9e1045fc9fb6b2a1eca03d231fd775ad127a44d1
Alexandre Pedrecal Campos <pedrecalc@gmail.com>
2021-05-19 17:49:23 -0300

    go study, class 26

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type deck []string
 
@@ -8,7 +11,7 @@ func newDeck() deck{
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -19,12 +22,16 @@ func newDeck() deck{
 	return cards;
 }
 
+func deal(d deck, handSize int) (deck, deck){
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
-// func (d deck) shuffle(){
-
-// }
+func (d deck) toString() string{
+	return strings.Join([]string(d), ",\n")
+}
