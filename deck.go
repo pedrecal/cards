aa12e8d4cfb6297e723ec42c3ddc836b4523d5d5
Alexandre Pedrecal Campos <pedrecalc@gmail.com>
2021-05-20 12:10:45 -0300

    add file save/load; suffle

--- deck.go
+++ deck.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -34,4 +38,30 @@ func (d deck) print() {
 
 func (d deck) toString() string{
 	return strings.Join([]string(d), ",\n")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func (d deck) newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffleDeck() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPostion := r.Intn(len(d) - 1)
+
+		d[i], d[newPostion] = d[newPostion], d[i]
+	}
 }
